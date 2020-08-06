1c60c4b1480bf5a61d1727b876dce7bd9ae725de
Genevieve Masioni <masionigenevieve@gmail.com>
2020-08-06 21:19:57 +0200

    Add Expertises section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,4 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
 	//The first argument are the elements to which the plugin shall be initialized
 	//The second argument has to be at least a empty object or a object with your desired options
 	OverlayScrollbars(document.querySelectorAll("body"), {className : "os-theme-dark" });
+
+	var siteHeader = document.querySelector(".site-header");
+	var siteContent = document.querySelector(".site-content");
+	// var siteMeta = document.querySelector(".site-metadata");
+	// siteMeta.style.maxHeight = window.innerHeight + "px";
+	siteContent.style.marginTop = `${siteHeader.clientHeight}px`;
 });
