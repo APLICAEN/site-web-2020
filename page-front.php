17ceb3fe5077d83c8a21753f1dce9094f00fe2b1
Genevieve Masioni <masionigenevieve@gmail.com>
2020-08-09 01:32:54 +0200

    Implement custom page template : expertise

---
+++ page-front.php
@@ -0,0 +1,409 @@
+<?php
+/* Template Name: Accueil */
+get_header(); ?>
+<section class="site-metadata">
+  <section class="metadata-info">
+    <section class="metadata-general">
+      <h2 class="metadata-name">Aplicaen association étudiants ingénieurs</h2>
+      <p class="metadata-description">
+        Mobilise les étudiants-ingénieurs de l’ENSICAEN pour accompagner
+        les entreprises, les centres de recherche et les entrepreneurs
+        dans leurs projets des domaines :
+      </p>
+    </section>
+    <section class="metadata-statistics">
+      <div class="statistic-wrapper">
+        <h3 class="statistic-number">8000</h3>
+        <p class="statistic-desc">
+          diplômés depuis 1976 pour nous conseiller
+        </p>
+      </div>
+      <div class="statistic-wrapper">
+        <h3 class="statistic-number">750</h3>
+        <p class="statistic-desc">
+          étudiants prêts à se mobiliser pour votre projet
+        </p>
+      </div>
+      <div class="statistic-wrapper">
+        <h3 class="statistic-number">14</h3>
+        <p class="statistic-desc">
+          ans d’expérience
+        </p>
+      </div>
+    </section>
+  </section>
+  <section class="metadata-cta">
+    <ul class="cta-expertises">
+      <li>Electronique et Physique Appliquée</li>
+      <li>Informatique</li>
+      <li>Matériaux-Chimie</li>
+    </ul>
+    <button type="button" class="cta-primary">Demander un devis</button>
+  </section>
+</section>
+<section class="site-section">
+  <div class="section-header">
+    <h2 class="section-title">Nos expertises</h2>
+  </div>
+  <div class="section-content">
+    <section class="expertises-wrapper">
+      <div class="card card-expertise">
+        <a href="#" class="card-link">
+          <div class="card-header">
+            <h4 class="expertise-name">Informatique</h4>
+          </div>
+          <div class="card-content">
+            <p class="expertise-desc">
+              Nous offrons un large spectre de technologies de pointe : 
+              développement web, mobile et logiciel, e-Paiement, cybersécurité
+              et multimédia.
+            </p>
+          </div>
+          <div class="card-footer">
+            <div class="expertise-cta">
+              <a href="#">En savoir plus</a>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="card card-expertise">
+        <a href="#" class="card-link">
+          <div class="card-header">
+            <h4 class="expertise-name">Electronique et Physique Appliquée</h4>
+          </div>
+          <div class="card-content">
+            <p class="expertise-desc">
+              Nos proposons des solutions innovantes pour l’ingénierie des capteurs,
+              du numérique et de l’énergie.
+            </p>
+          </div>
+          <div class="card-footer">
+            <div class="expertise-cta">
+              <a href="#">En savoir plus</a>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="card card-expertise">
+        <a href="#" class="card-link">
+          <div class="card-header">
+            <h4 class="expertise-name">Matériaux-Chimie</h4>
+          </div>
+          <div class="card-content">
+            <p class="expertise-desc">
+              Nous apportons des réponses aux enjeux sociétaux du 21e siècle :
+              défis énergétiques, développement durable ou santé.
+            </p>
+          </div>
+          <div class="card-footer">
+            <div class="expertise-cta">
+              <a href="#">En savoir plus</a>
+            </div>
+          </div>
+        </a>
+      </div>
+    </section>
+  </div>
+</section>
+<section class="site-section">
+  <div class="section-header">
+    <h2 class="section-title">Nos prestations</h2>
+  </div>
+  <div class="section-content">
+    <section class="prestations-wrapper">
+      <section class="prestations-column">
+        <div class="card card-prestation">
+          <a href="#" class="card-link">
+            <h6 class="prestation-name">Base de données</h6>
+          </a>
+        </div>
+        <div class="card card-prestation">
+          <a href="#" class="card-link">
+            <h6 class="prestation-name">Base de données</h6>
+          </a>
+        </div>
+        <div class="card card-prestation">
+          <a href="#" class="card-link">
+            <h6 class="prestation-name">Base de données</h6>
+          </a>
+        </div>
+      </section>
+      <section class="prestations-column">
+        <div class="card card-prestation">
+          <a href="#" class="card-link">
+            <h6 class="prestation-name">Base de données</h6>
+          </a>
+        </div>
+        <div class="card card-prestation">
+          <a href="#" class="card-link">
+            <h6 class="prestation-name">Base de données</h6>
+          </a>
+        </div>
+        <div class="card card-prestation">
+          <a href="#" class="card-link">
+            <h6 class="prestation-name">Base de données</h6>
+          </a>
+        </div>
+        <div class="card card-prestation">
+          <a href="#" class="card-link">
+            <h6 class="prestation-name">Base de données</h6>
+          </a>
+        </div>
+      </section>
+      <section class="prestations-column">
+        <div class="card card-prestation">
+          <a href="#" class="card-link">
+            <h6 class="prestation-name">Base de données</h6>
+          </a>
+        </div>
+        <div class="card card-prestation">
+          <a href="#" class="card-link">
+            <h6 class="prestation-name">Base de données</h6>
+          </a>
+        </div>
+        <div class="card card-prestation">
+          <a href="#" class="card-link">
+            <h6 class="prestation-name">Base de données</h6>
+          </a>
+        </div>
+      </section>
+    </section>
+  </div>
+  <div class="section-footer">
+    <button type="button" class="prestations-cta">Toutes nos prestations</button>
+  </div>
+</section>
+<section class="site-section hero-section">
+  <div class="section-header">
+    <h2 class="section-title">Notre processus</h2>
+  </div>
+  <div class="section-content">
+    <section class="processus-wrapper">
+      <div class="card card-processus">
+        <div class="card-header">
+          <img src="./assets/media/images/icon-startup.png" alt="Icon startup" class="card-icon">
+          <h6 class="prestation-name">Appel d'offre</h6>
+        </div>
+        <div class="card-content">
+          <p class="expertise-desc">
+            Vous pouvez soumettre votre offre d’étude directement sur
+            notre site Internet, par e-mail ou par courrier.
+          </p>
+        </div>
+      </div>
+      <div class="card card-processus">
+        <div class="card-header">
+          <img src="./assets/media/images/icon-startup.png" alt="Icon startup" class="card-icon">
+          <h6 class="prestation-name">Contact</h6>
+        </div>
+        <div class="card-content">
+          <p class="expertise-desc">
+            Nous recontactons sous 48 h pour confirmer l’adéquation de
+            l’étude avec nos domaines de compétences.
+          </p>
+        </div>
+      </div>
+      <div class="card card-processus">
+        <div class="card-header">
+          <img src="./assets/media/images/icon-startup.png" alt="Icon startup" class="card-icon">
+          <h6 class="prestation-name">Analyse</h6>
+        </div>
+        <div class="card-content">
+          <p class="expertise-desc">
+            Un chef de projet vous accompagne dans la rédaction d’un
+            cahier des charges.
+            Nous établissons un planning et un budget.
+          </p>
+        </div>
+      </div>
+      <div class="card card-processus">
+        <div class="card-header">
+          <img src="./assets/media/images/icon-startup.png" alt="Icon startup" class="card-icon">
+          <h6 class="prestation-name">Recrutement</h6>
+        </div>
+        <div class="card-content">
+          <p class="expertise-desc">
+            Nous recrutons, parmi les 750 élèves de l’ENSICAEN, des intervenants
+            pour constituer l’équipe qui répondra au mieux à vos besoins.
+          </p>
+        </div>
+      </div>
+      <div class="card card-processus">
+        <div class="card-header">
+          <img src="./assets/media/images/icon-startup.png" alt="Icon startup" class="card-icon">
+          <h6 class="prestation-name">Suivi</h6>
+        </div>
+        <div class="card-content">
+          <p class="expertise-desc">
+            Nous assurons un suivi régulier de l’étude dès son lancement
+            pour nous adapter à l’évolution de vos besoins et y répondre
+            de façon pertinente.
+          </p>
+        </div>
+      </div>
+      <div class="card card-processus">
+        <div class="card-header">
+          <img src="./assets/media/images/icon-startup.png" alt="Icon startup" class="card-icon">
+          <h6 class="prestation-name">Clôture</h6>
+        </div>
+        <div class="card-content">
+          <p class="expertise-desc">
+            Nous vous présentons nos conclusions et vous remettons les livrables.
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+</section>
+<section class="site-section">
+  <div class="section-header">
+    <h2 class="section-title">Nos références</h2>
+  </div>
+  <div class="section-content">
+    <section class="credentials-wrapper">
+      <p>
+        Nous faire confiance, c'est faire le même choix que nos anciens
+        clients et partenaires
+      </p>
+      <img src="./assets/media/images/credentials.png" alt="Partenaires">
+    </section>
+  </div>
+  <div class="section-footer">
+    <button type="button" class="prestations-cta">Devenir partenaire</button>
+  </div>
+</section>
+<section class="site-section">
+  <div class="section-header">
+    <h2 class="section-title">Ils en parlent le mieux</h2>
+  </div>
+  <div class="section-content">
+    <section class="testimonials-wrapper">
+      <div class="card card-testimonial">
+        <div class="card-content">
+          <p class="expertise-desc">
+            Réactifs et dynamiques, les membres d’APLICAEN sauront répondre
+            avec professionnalisme et pertinence à vos besoins en mettant
+            à profit la formation dont ils bénéficient à l’ENSICAEN.
+          </p>
+        </div>
+        <div class="card-footer">
+          <img src="./assets/media/images/info.jpg" alt="Partenaires" class="author-picture">
+          <p class="author-name">J.F. Hamet</p>
+          <p class="author-position">Directeur ENSICAEN</p>
+        </div>
+      </div>
+      <div class="card card-testimonial">
+        <div class="card-content">
+          <p class="expertise-desc">
+            Réactifs et dynamiques, les membres d’APLICAEN sauront répondre
+            avec professionnalisme et pertinence à vos besoins en mettant
+            à profit la formation dont ils bénéficient à l’ENSICAEN.
+          </p>
+        </div>
+        <div class="card-footer">
+          <img src="./assets/media/images/info.jpg" alt="Partenaires" class="author-picture">
+          <p class="author-name">J.F. Hamet</p>
+          <p class="author-position">Directeur ENSICAEN</p>
+        </div>
+      </div>
+      <div class="card card-testimonial">
+        <div class="card-content">
+          <p class="expertise-desc">
+            Réactifs et dynamiques, les membres d’APLICAEN sauront répondre
+            avec professionnalisme et pertinence.
+          </p>
+        </div>
+        <div class="card-footer">
+          <img src="./assets/media/images/info.jpg" alt="Partenaires" class="author-picture">
+          <p class="author-name">J.F. Hamet</p>
+          <p class="author-position">Directeur ENSICAEN</p>
+        </div>
+      </div>
+    </section>
+  </div>
+  <div class="section-footer">
+    <button type="button" class="prestations-cta">Tous nos témoignages</button>
+  </div>
+</section>
+<section class="site-section">
+  <div class="section-header">
+    <h2 class="section-title">Notre équipe</h2>
+  </div>
+  <div class="section-content full-width">
+    <div class="image-overlay-wrapper" style="background-image: url('./assets/media/images/info.jpg') ;">
+    </div>
+  </div>
+  <div class="section-footer">
+    <button type="button" class="section-cta">Voir l'équipe</button>
+  </div>
+</section>
+<section class="site-section">
+  <div class="section-header">
+    <h2 class="section-title">Notre actualité</h2>
+  </div>
+  <div class="section-content">
+    <section class="recommended-wrapper">
+      <article class="card card-recommended">
+        <div class="recommended-header">
+          <a href="#" class="card-link">
+            <div class="recommended-thumbnail-wrapper">
+              <img src="./assets/media/images/info.jpg" alt="Icon startup" class="recommended-thumbnail">
+            </div>
+          </a>
+          <h6 class="recommended-publish-date">25/03/2020</h6>
+          <a href="#" class="card-link">
+            <h4 class="recommended-title">5 bonnes raisons de faire appel à une JE</h4>
+          </a>
+        </div>
+        <div class="recommended-content">
+          <p>
+            culpa dolore pariatur quis consequat ex deserunt cupidatat.
+            Aute do cillum laborum ut occaecat. Officia
+          </p>
+        </div>
+      </article>
+      <article class="card card-recommended">
+        <div class="recommended-header">
+          <a href="#" class="card-link">
+            <div class="recommended-thumbnail-wrapper">
+              <img src="./assets/media/images/info.jpg" alt="Icon startup" class="recommended-thumbnail">
+            </div>
+          </a>
+          <h6 class="recommended-publish-date">25/03/2020</h6>
+          <a href="#">
+            <h4 class="recommended-title">5 bonnes raisons de faire appel à une JE</h4>
+          </a>
+        </div>
+        <div class="recommended-content">
+          <p>
+            culpa dolore pariatur quis consequat ex deserunt cupidatat.
+            Aute do cillum laborum ut occaecat. Officia
+          </p>
+        </div>
+      </article>
+      <article class="card card-recommended">
+        <div class="recommended-header">
+          <a href="#" class="card-link">
+            <div class="recommended-thumbnail-wrapper">
+              <img src="./assets/media/images/info.jpg" alt="Icon startup" class="recommended-thumbnail">
+            </div>
+          </a>
+          <h6 class="recommended-publish-date">25/03/2020</h6>
+          <a href="#">
+            <h4 class="recommended-title">5 bonnes raisons de faire appel à une JE</h4>
+          </a>
+        </div>
+        <div class="recommended-content">
+          <p>
+            culpa dolore pariatur quis consequat ex deserunt cupidatat.
+            Aute do cillum laborum ut occaecat. Officia
+          </p>
+        </div>
+      </article>
+    </section>
+  </div>
+  <div class="section-footer">
+    <button type="button">Toute notre actualité</button>
+  </div>
+</section>
+<?php get_footer(); ?>
--- page.php
+++ page.php
@@ -12,35 +12,4 @@
   <?php the_content(); ?>
 </article>
 <?php endwhile; endif; ?>
-<!--  MODELE DE PAGE "expertise" seulement : sélection trois prestations -->
-<section class="site-section">
-  <div class="section-header">
-    <h2 class="section-title">Nos prestations associées</h2>
-  </div>
-  <div class="section-content">
-    <section class="recommended-wrapper">
-      <article class="card card-recommended">
-        <div class="recommended-header">
-          <a href="#" class="card-link">
-            <div class="recommended-thumbnail-wrapper">
-              <img src="./assets/media/images/info.jpg" alt="Icon startup" class="recommended-thumbnail">
-            </div>
-          </a>
-          <a href="#" class="card-link">
-            <h4 class="recommended-title">Rédaction de contenu</h4>
-          </a>
-        </div>
-        <div class="recommended-content">
-          <p>
-            culpa dolore pariatur quis consequat ex deserunt cupidatat.
-            Aute do cillum laborum ut occaecat. Officia
-          </p>
-        </div>
-      </article>
-    </section>
-  </div>
-  <div class="section-footer">
-    <button type="button">Voir plus de prestations</button>
-  </div>
-</section>
 <?php get_footer(); ?>
