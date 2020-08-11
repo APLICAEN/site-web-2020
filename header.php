8871252d4e766aaf126f035183e47a843e3e9123
Genevieve Masioni <masionigenevieve@gmail.com>
2020-08-11 06:41:57 +0200

    Fix header call to action url issue

--- header.php
+++ header.php
@@ -53,7 +53,7 @@
     </ul>
   </nav>
   <section class="call-to-action">
-    <a href="<?php bloginfo("url"); echo '/' . get_page_uri(get_theme_mod("header_cta_file")); ?>"
+    <a href="<?php echo wp_get_attachment_url(get_theme_mod("header_cta_file")); ?>"
     target="_blank" lass="cta-link">
     <button type="button" class="btn-secondary-cta"><?php echo get_theme_mod("header_cta_text"); ?></button>
     </a>
